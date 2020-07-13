c6654e232aeb6da25b5ab38ec6beae411e5f36b0
Hyunwoong Ko <gusdnd852@naver.com>
2020-07-13 09:42:47 +0900

    초기화

--- dockerfiles/cpu/run_daemon.sh
+++
@@ -1,3 +0,0 @@
-sudo service docker stop
-sudo dockerd -H xxx.xxx.xxx.xxx:2375
-# input your server ip address
--- dockerfiles/gpu/run_docker.sh
+++
@@ -1,8 +0,0 @@
-sudo docker pull gusdnd852/blog:latest
-
-sudo nvidia-docker run \
-	-it \
-	-p 8888:8888 \
-	-v /home:/home \
-	gusdnd852/blog:latest \
-	bin/bash
